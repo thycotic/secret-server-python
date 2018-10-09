8b3d28fa87cbf973605334df852ca742e728fc54
Ali Falahi <ali@afalahi.com>
2018-10-08 20:02:30 -0400

    added remove client config method

--- secret_server/config.py
+++ secret_server/config.py
@@ -1,45 +1,32 @@
 # -*- coding: utf-8 -*-
 
+import requests
+import json
+import platform
 import os
+from uuid import uuid4
+
+
 class Config:
-    SDK_CONFIG = {
-        'url' : '',
-        'rule' : '',
-        'key' : '',
-        'cache_strategy' : 0,
-        'cache_age' : 0
-    }
-
+    CLIENT_CONFIG = {
+        'clientId' : uuid4(),
+        'description' : 'Machine: {node}, OS: {system} - Python {version}'.format(node = platform.node(), system = platform.system(), version = platform._sys_version()[1]),
+        'name' : platform.node(),
+        'ruleName' : '' or os.environ.get('RULE_NAME'),
+        'onboardingKey' : '' or os.environ.get('RULE_KEY')
+    }   
+    BASE_URL = '' or os.environ.get('SECRET_SERVER_BASE_URL')
+    
     @classmethod
-    def get_sdk_file_path(cls):
-        sdk_file = os.path.join(cls.SDK_CONFIG['path'], 'tss')
-        return os.path.join(cls.SDK_CONFIG['path'], 'tss.exe') if(not os.path.isfile(sdk_file)) else sdk_file
-
+    def register_client(cls):
+        resp = requests.post(cls.BASE_URL+"/api/v1/sdk-client-accounts", data = cls.CLIENT_CONFIG)
+        with open("creds.json", "w") as outfile:
+            json.dump(resp.json(), outfile)
+        resp.close()
+    
     @classmethod
-    def get_strategy(cls):
-        strat_dict = ['Never Cache', 'Server Then Cache', 'Cache Then Server', 'Cache Then Server Fallback on Expired Cache']
-
-        if (not isinstance(cls.SDK_CONFIG['cache_strategy'], int) or cls.SDK_CONFIG['cache_strategy'] < 0
-                or cls.SDK_CONFIG['cache_strategy'] >= strat_dict.__len__()):
-            raise ValueError('Invalid cache strategy. Please look at SDK manual for cache settings information.')
-
-        return strat_dict[cls.SDK_CONFIG['cache_strategy']]
-
-    @classmethod
-    def has_valid_path(cls):
-        return os.path.isfile(cls.get_sdk_file_path())
-
-    @classmethod
-    def has_valid_cache(cls):
-        cls.get_strategy()
-        if (not isinstance(cls.SDK_CONFIG['cache_age'], int) or
-                (cls.SDK_CONFIG['cache_strategy'] > 0 and cls.SDK_CONFIG['cache_age'] <= 0)):
-            raise ValueError('Cache age must be a positive integer when trying to set cache')
-        return True
-
-    @classmethod
-    def set_config_from_env(cls):
-        cls.SDK_CONFIG['path'] = os.environ.get('SDK_CLIENT_PATH')
-        cls.SDK_CONFIG['url'] = os.environ.get('SECRET_SERVER_URL')
-        cls.SDK_CONFIG['rule'] = os.environ.get('SDK_CLIENT_RULE')
-        cls.SDK_CONFIG['key'] = os.environ.get('SDK_CLIENT_KEY')
+    def remove_client(cls):
+        if os.path.exists("creds.json"):
+            os.remove("creds.json")
+        else:
+            print("The file does not exist")
