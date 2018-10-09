00e9f01d9da370a219f78d7ab51c00f08bd14743
Ali Falahi <ali@afalahi.com>
2018-10-09 07:53:12 -0400

    added token and secret classes

--- secret_server/commands.py
+++ secret_server/commands.py
@@ -1,69 +1,30 @@
 # -*- coding: utf-8 -*-
-
-from subprocess import Popen, PIPE
-
 from secret_server.config import Config
-from secret_server.configure import Configure
-#from secret_server.sdk_client import SDK_Client
+import requests
+import json
 
-class Commands:
-    @classmethod
-    def execute(cls, args):
-        if Config.has_valid_path():
-            p = Popen(' '.join((Config.get_sdk_file_path(),) + args), shell=True, stdout=PIPE, stderr=PIPE)
-            stdout, stderr = p.communicate()
-            statuscode = p.returncode
-
-            if(statuscode != 0):
-                raise ValueError(stdout.decode('utf-8') + " Thycotic SDK configuration is invalid.")
-        else:
-            raise ValueError('SDK client path is invalid')
-
-        return [stdout.decode('utf-8'), stderr, statuscode]
+class AccessToken:
 
     @classmethod
-    def initialize(cls):
-        if Configure.CLIENT_CONFIG['url'] is None:
-            raise ValueError('Secret Server URL is not set')
-        Configure.register_client()
-        command = (' -e ', ' -u ', Config.SDK_CONFIG['url'])
+    def get_token(cls):
+        __creds = {"grant_type" : "client_credentials"}
 
-        if Config.SDK_CONFIG['rule']:
-            command += (' -r ', Config.SDK_CONFIG['rule'])
+        with open("creds.json","r") as outfile:
+            load = json.load(outfile)
+            __creds["client_id"] = "sdk-client-"+load["clientId"]
+            __creds["client_secret"] = load["clientSecret"]
 
-        if Config.SDK_CONFIG['key']:
-            command += (' -k ', Config.SDK_CONFIG['key'])
+        resp = requests.post(Config.BASE_URL+"/oauth2/token", data=__creds)
+        return resp.json()["access_token"]
 
-        return cls.execute(('init',) + command)[0]
-
+class Secret:
+    
     @classmethod
-    def get_secret(cls, id, **kwargs):
-        if not isinstance(id, int) or id <= 0 :
-            raise ValueError('id must be a positive integer')
-
-        command = ('secret -s ', id.__str__())
-
-        if 'field' in kwargs.keys():
-            command += (' -ad ',) if(kwargs['field'] == 'all') else (' -f ', kwargs['field'])
-
-        result = cls.execute(command)
-
-        return result[0]
-
-    @classmethod
-    def set_cache(cls):
-        command = None
-        if(Config.has_valid_cache()):
-            command = (' cache -s ', Config.SDK_CONFIG['cache_strategy'].__str__())
-
-            if Config.SDK_CONFIG['cache_age'] is not None:
-                command += (' -a ', Config.SDK_CONFIG['cache_age'].__str__())
-        return cls.execute(command)[0]
-
-    @classmethod
-    def clear_cache(cls):
-        return cls.execute(('cache', '-b'))[0]
-
-    @classmethod
-    def remove(cls):
-        return cls.execute(('remove', '-c'))[0]
+    def get_secret(cls, id):
+        if type(id) == int:
+            id = str(id)
+        token = AccessToken.get_token()
+        headers = {"Authorization" : "bearer "+token, "Content-Type" : "application/json"}
+        uri = Config.BASE_URL+"/api/v1/secrets/"
+        resp = requests.get(uri+id,headers=headers,)
+        return resp.json()
