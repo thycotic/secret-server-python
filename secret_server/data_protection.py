4d8ac2424092ced2fe8ecad8fbd86ac552d35a49
afalahi <ali@afalahi.com>
2018-10-10 16:16:30 -0400

    added additional testing files to ignore

--- secret_server/data_protection.py
+++ secret_server/data_protection.py
@@ -1,3 +1,22 @@
 import os
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+from base64 import b64encode
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.ciphers import (Cipher, algorithms, modes)
+
+
+class DataProtection:
+
+    @classmethod
+    def get_master_key(cls):
+        try:
+            if not os.path.exists('masterKey.config'):
+                random = os.urandom(32)
+                master_key = b64encode(random)
+                print(master_key)
+        except:
+            master_key
+
+DataProtection().get_master_key()
