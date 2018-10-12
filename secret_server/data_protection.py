c863a60cff66546c76f1630fa57398689df57fd5
Ali Falahi <ali@afalahi.com>
2018-10-11 22:24:23 -0400

    added kdf and encyrpt method

--- secret_server/data_protection.py
+++ secret_server/data_protection.py
@@ -8,13 +8,15 @@ from cryptography.hazmat.primitives.ciphers import (Cipher, algorithms, modes)
 
 
 class DataProtection:
+    SALT = os.urandom(64)
+    FILE_Path = "creds.json"
 
     @classmethod
     def get_master_key(cls):
         path = "masterKey.config"
         try:
-            if not os.path.exists('masterKey.config'):
-                master_key = b64encode(os.urandom(32)).decode('utf-8')
+            if not os.path.exists(path):
+                master_key = b64encode(os.urandom(32)).decode('UTF-8')
                 open(path, "w").write(master_key)
             else:
                 master_key = open(path).read()
@@ -22,8 +24,32 @@ class DataProtection:
         except Exception as err:
             print(err)
             raise
-    
-    @classmethod
-    def encrypt(cls):
 
-DataProtection().get_master_key()
+    @classmethod
+    def get_key(cls, salt):
+        # derive key from master key        
+        backend = default_backend()
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA512(),
+            length=32,
+            salt= salt,
+            iterations=2145,
+            backend=backend
+        )
+        return kdf.derive(b64encode(cls.get_master_key()))
+
+    @classmethod
+    def encrypt(cls, data):
+        iv = os.urandom(16)
+        encryptor = Cipher(
+            algorithms.AES(cls.get_key(salt=cls.SALT)),
+            modes.GCM(iv),
+            backend = default_backend()
+        ).encryptor()
+
+        ciphertext = encryptor.update(data) + encryptor.finalize()
+
+
+# # DataProtection().get_master_key()
+
+# print(DataProtection().get_key())
