012f4fc967b5d522b68c9fbb766f74dd877cb179
Ali Falahi <ali@afalahi.com>
2018-10-14 10:15:12 -0400

    Implemented aes gcm encryption

--- secret_server/data_protection.py
+++ secret_server/data_protection.py
@@ -1,6 +1,6 @@
 import os
 
-from base64 import b64encode
+from base64 import (b64encode, b64decode)
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
@@ -16,7 +16,7 @@ class DataProtection:
         path = "masterKey.config"
         try:
             if not os.path.exists(path):
-                master_key = b64encode(os.urandom(32)).decode('UTF-8')
+                master_key = b64encode(os.urandom(32))
                 open(path, "w").write(master_key)
             else:
                 master_key = open(path).read()
@@ -36,20 +36,56 @@ class DataProtection:
             iterations=2145,
             backend=backend
         )
-        return kdf.derive(b64encode(cls.get_master_key()))
+        try:
+            return kdf.derive(cls.get_master_key())
+        except Exception as e:
+            raise Exception(e.message)
 
     @classmethod
     def encrypt(cls, data):
+        if type(data) is dict:
+            data = str(data)
+
+        salt = cls.SALT
         iv = os.urandom(16)
         encryptor = Cipher(
-            algorithms.AES(cls.get_key(salt=cls.SALT)),
+            algorithms.AES(cls.get_key(salt=salt)),
             modes.GCM(iv),
             backend = default_backend()
         ).encryptor()
 
-        ciphertext = encryptor.update(data) + encryptor.finalize()
+        try:
+            ciphertext = encryptor.update(data) + encryptor.finalize()
+            tag = encryptor.tag
+        except Exception as e:
+            raise Exception(e.message)
+        
+        payload = salt + iv + tag + ciphertext
 
+        try:
+            open("test.json" , "w").write(b64encode(payload))
+        except IOError as e:
+            raise IOError("Couldn't Save creds.json: " + e.message)
 
-# # DataProtection().get_master_key()
+    @classmethod
+    def decrypt(cls):
+        try:
+            raw = b64decode(open('test.json', 'r').read())
+        except IOError as e:
+           "Couldn't Open creds.json: " + e.message
+           raise
+        #slice the bytes to get the salt, iv, tag, and the ciphertext
+        salt =  raw[:64]
+        iv = raw[64:80]
+        tag = raw[80:96]
+        ciphertext = raw[96:]
 
-# print(DataProtection().get_key())
+        decryptor = Cipher(
+            algorithms.AES(cls.get_key(salt=salt)),
+            modes.GCM(iv, tag),
+            backend=default_backend()
+        ).decryptor()
+        try:
+            return decryptor.update(ciphertext) + decryptor.finalize()
+        except Exception as e:
+            raise Exception(e.message)
