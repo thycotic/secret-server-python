80bd3014129a6c9e44238cd12964fc769dfcba2b
Ali Falahi <ali@afalahi.com>
2018-10-14 21:07:03 -0400

    using encryption now. the URL is loaded from a json file after init.

--- secret_server/commands.py
+++ secret_server/commands.py
@@ -1,44 +1,49 @@
 # -*- coding: utf-8 -*-
-from secret_server.config import Config
+# from secret_server.config import Config
 import requests
 import json
+import os
+
+from secret_server.data_protection import DataProtection
+
+if os.path.exists("client_info.json"):
+    with open("client_info.json", "r") as outfile:
+        BASE_URL = json.load(outfile)["endpoint"]
+        URL = "{base_url}/api/v1/secrets".format(base_url=BASE_URL)
 
 class AccessToken:
 
     @classmethod
     def get_token(cls):
-        with open("creds.json","r") as outfile:
-            load = json.load(outfile)
-
-        resp = requests.post(Config.BASE_URL+"/oauth2/token", data=load)
+        creds = DataProtection().decrypt()
+        resp = requests.post(BASE_URL+"/oauth2/token", data=creds)
         resp.close()
+        creds = None
         return resp.json()["access_token"]
 
 class Secret:
-    
+
     @classmethod
     def __get_headers(cls):
         headers = {"Authorization" : "bearer {token}".format(token=AccessToken.get_token()), "Content-Type" : "application/json"}
         return headers
 
     @classmethod
-    def get(cls, id):
-        if type(id) == int:
-            id = str(id)
+    def get(cls, s_id):
+        if type(s_id) == int:
+            s_id = str(s_id)
 
-        url =  "{base_url}/api/v1/secrets".format(base_url=Config.BASE_URL)
-        uri = '{url}/{id}'.format(url=url,id=id)
+        uri = "{url}/{id}".format(url=URL,id=s_id)
         resp = requests.get(uri,headers=cls.__get_headers())
         resp.close()
         return resp.json()
 
     @classmethod
-    def get_field(cls, id, field):
-        if type(id) == int:
-            id = str(id)
+    def get_field(cls, s_id, field):
+        if type(s_id) == int:
+            s_id = str(s_id)
             
-        url =  "{base_url}/api/v1/secrets".format(base_url=Config.BASE_URL)
-        uri = '{url}/{id}/fields/{field}'.format(url=url, id=id, field=field)
+        uri = "{url}/{id}/fields/{field}".format(url=URL, id=s_id, field=field)
         resp = requests.get(uri, headers=cls.__get_headers())
         resp.close()
         return resp.json()
