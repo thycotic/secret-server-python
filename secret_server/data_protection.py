9dc24fab3c21714c7a99d1db269fbcb888d15111
Ali Falahi <ali@afalahi.com>
2018-10-16 08:59:06 -0400

    spacing

--- secret_server/data_protection.py
+++ secret_server/data_protection.py
@@ -9,7 +9,6 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives.ciphers import (Cipher, algorithms, modes)
 
-
 class DataProtection:
     SALT = os.urandom(64)
 
