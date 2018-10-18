f1b13dfabef085a72f30d7c4c03c77e53590d810
afalahi <ali@afalahi.com>
2018-10-18 13:26:31 -0400

    moved uri builing to methods. later will have a function instead

--- secret_server/commands.py
+++ secret_server/commands.py
@@ -2,19 +2,16 @@
 import requests
 import json
 import os
-import time
 
-from secret_server.config import Config
 from secret_server.data_protection import DataProtection
 from secret_server.api_response_handler import HandleApiResponse as handler
-
-BASE_URL = DataProtection().decrypt(Config.CLIENT_PATH)["endpoint"] if os.path.exists(Config.CLIENT_PATH) else Config.BASE_URL
-URL = "{base_url}/api/v1/secrets".format(base_url=BASE_URL)
+from secret_server.config import Config
 
 class AccessToken:
 
     @classmethod
     def get_token(cls):
+        BASE_URL = DataProtection().decrypt(Config.CLIENT_PATH)["endpoint"] if os.path.exists(Config.CLIENT_PATH) else Config.BASE_URL
         creds = DataProtection().decrypt(Config.CREDS_PATH)
         resp = requests.post(BASE_URL+"/oauth2/token", data=creds, verify=False)
         creds = None
@@ -32,7 +29,8 @@ class Secret:
     def get(cls, s_id):
         if type(s_id) == int:
             s_id = str(s_id)
-
+        BASE_URL = DataProtection().decrypt(Config.CLIENT_PATH)["endpoint"] if os.path.exists(Config.CLIENT_PATH) else Config.BASE_URL or Config.BASE_URL
+        URL = "{base_url}/api/v1/secrets".format(base_url=BASE_URL)
         uri = "{url}/{id}".format(url=URL,id=s_id)
         resp = requests.get(uri,headers=cls.__get_headers(), verify=False)
         return handler(resp)
@@ -41,7 +39,8 @@ class Secret:
     def get_field(cls, s_id, field):
         if type(s_id) == int:
             s_id = str(s_id)
-            
+        BASE_URL = DataProtection().decrypt(Config.CLIENT_PATH)["endpoint"] if os.path.exists(Config.CLIENT_PATH) else Config.BASE_URL or Config.BASE_URL
+        URL = "{base_url}/api/v1/secrets".format(base_url=BASE_URL)
         uri = "{url}/{id}/fields/{field}".format(url=URL, id=s_id, field=field)
         resp = requests.get(uri, headers=cls.__get_headers(), verify=False)
         return handler(resp)
